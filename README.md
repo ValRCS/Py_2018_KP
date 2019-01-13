99777f7a62b071e798cc62b8fa4bfbecf8b0dbf3
Rihards Skuja <rhssk@users.noreply.github.com>
2019-01-13 09:51:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# pq
+
+Fetch listings from different online marketplaces.
+
+Currently supported on: Arch Linux and Ubuntu 18.10.
+
+## Installing
+* `make install`
+
+## Running
+* `./pq.sh`
+
+## Testing
+* `make test`
+
+## Documentation
+* `make docs`
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="pq",
+    version="0.1.0",
+    license="MIT",
+    description="Fetch listings from different online marketplaces",
+    long_description=long_description,
+    url="https://github.com/rhssk/Py_2018_KP",
+    author="Rihards Skuja",
+    author_email="rssk@protonmail.com",
+    packages=find_packages(),
+    install_requires=["PySide2", "requests"],
+    tests_require=["pytest"],
+)
